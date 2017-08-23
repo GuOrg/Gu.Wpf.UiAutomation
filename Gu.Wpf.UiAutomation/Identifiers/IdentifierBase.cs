9aeff7abd5b24bb165cddc06c4b2a47961c85aaa
Johan Larsson <johan_gavle1@hotmail.com>
2017-08-23 09:19:54 +0200

    Remove IdentifiersHolder

--- Gu.Wpf.UiAutomation/Identifiers/IdentifierBase.cs
+++ Gu.Wpf.UiAutomation/Identifiers/IdentifierBase.cs
@@ -1,6 +1,7 @@
 ﻿namespace Gu.Wpf.UiAutomation.Identifiers
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
 
     /// <summary>
@@ -9,32 +10,33 @@
     public abstract class IdentifierBase : IEquatable<IdentifierBase>
     {
         /// <summary>
-        /// Class which capsules all identifiers which can be used for an automation library
+        /// Dictionary which holds all known properties
         /// </summary>
-        private class IdentifiersHolder
+        private static readonly ConcurrentDictionary<int, PropertyId> PropertyCache = new ConcurrentDictionary<int, PropertyId>();
+
+        /// <summary>
+        /// Dictionary which holds all known events
+        /// </summary>
+        private static readonly ConcurrentDictionary<int, EventId> EventCache = new ConcurrentDictionary<int, EventId>();
+
+        /// <summary>
+        /// Dictionary which holds all known patterns
+        /// </summary>
+        private static readonly ConcurrentDictionary<int, PatternId> PatternCache = new ConcurrentDictionary<int, PatternId>();
+
+        /// <summary>
+        /// Dictionary which holds all known text attributes
+        /// </summary>
+        private static readonly ConcurrentDictionary<int, TextAttributeId> TextAttributeCache = new ConcurrentDictionary<int, TextAttributeId>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentifierBase"/> class.
+        /// </summary>
+        protected IdentifierBase(int id, string name)
         {
-            /// <summary>
-            /// Dictionary which holds all known properties
-            /// </summary>
-            public readonly Dictionary<int, PropertyId> PropertyDict = new Dictionary<int, PropertyId>();
-
-            /// <summary>
-            /// Dictionary which holds all known events
-            /// </summary>
-            public readonly Dictionary<int, EventId> EventDict = new Dictionary<int, EventId>();
-
-            /// <summary>
-            /// Dictionary which holds all known patterns
-            /// </summary>
-            public readonly Dictionary<int, PatternId> PatternDict = new Dictionary<int, PatternId>();
-
-            /// <summary>
-            /// Dictionary which holds all known text attributes
-            /// </summary>
-            public readonly Dictionary<int, TextAttributeId> TextAttributeDict = new Dictionary<int, TextAttributeId>();
+            this.Id = id;
+            this.Name = name;
         }
-
-        private static readonly IdentifiersHolder Identifiers = new IdentifiersHolder();
 
         /// <summary>
         /// The native id of the identifier
@@ -45,12 +47,6 @@
         /// A readable name for the identifier
         /// </summary>
         public string Name { get; }
-
-        protected IdentifierBase(int id, string name)
-        {
-            this.Id = id;
-            this.Name = name;
-        }
 
         /// <inheritdoc/>
         public bool Equals(IdentifierBase other)
@@ -78,27 +74,27 @@
 
         protected static PropertyId RegisterProperty(int id, string name)
         {
-            return Register(id, Identifiers.PropertyDict, () => new PropertyId(id, name));
+            return PropertyCache.GetOrAdd(id, x => new PropertyId(id, name));
         }
 
         protected static EventId RegisterEvent(int id, string name)
         {
-            return Register(id, Identifiers.EventDict, () => new EventId(id, name));
+            return EventCache.GetOrAdd(id, x => new EventId(id, name));
         }
 
         protected static PatternId RegisterPattern(int id, string name, PropertyId availabilityProperty)
         {
-            return Register(id, Identifiers.PatternDict, () => new PatternId(id, name, availabilityProperty));
+            return PatternCache.GetOrAdd(id, x => new PatternId(x, name, availabilityProperty));
         }
 
         protected static TextAttributeId RegisterTextAttribute(int id, string name)
         {
-            return Register(id, Identifiers.TextAttributeDict, () => new TextAttributeId(id, name));
+            return TextAttributeCache.GetOrAdd(id, x => new TextAttributeId(x, name));
         }
 
         protected static PropertyId FindProperty(int id)
         {
-            if (Identifiers.PropertyDict.TryGetValue(id, out var propertyId))
+            if (PropertyCache.TryGetValue(id, out var propertyId))
             {
                 return propertyId;
             }
@@ -108,7 +104,7 @@
 
         protected static EventId FindEvent(int id)
         {
-            if (Identifiers.EventDict.TryGetValue(id, out var eventId))
+            if (EventCache.TryGetValue(id, out var eventId))
             {
                 return eventId;
             }
@@ -118,7 +114,7 @@
 
         protected static PatternId FindPattern(int id)
         {
-            if (Identifiers.PatternDict.TryGetValue(id, out var patternId))
+            if (PatternCache.TryGetValue(id, out var patternId))
             {
                 return patternId;
             }
@@ -128,7 +124,7 @@
 
         protected static TextAttributeId FindTextAttribute(int id)
         {
-            if (Identifiers.TextAttributeDict.TryGetValue(id, out var textAttributeId))
+            if (TextAttributeCache.TryGetValue(id, out var textAttributeId))
             {
                 return textAttributeId;
             }
--- Gu.Wpf.UiAutomation/Identifiers/PropertyId.cs
+++ Gu.Wpf.UiAutomation/Identifiers/PropertyId.cs
@@ -24,7 +24,7 @@
         }
 
         /// <summary>
-        /// Returs a condition for this property with the given value
+        /// Returns a condition for this property with the given value
         /// </summary>
         public PropertyCondition GetCondition(object value)
         {
