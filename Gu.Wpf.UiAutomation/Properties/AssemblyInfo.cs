d11362cd6d5aa4f32f35e4ebe3d582aee2f413b6
Johan Larsson <johan_gavle1@hotmail.com>
2017-08-25 14:13:35 +0200

    paket.template WIP

--- Gu.Wpf.UiAutomation/Properties/AssemblyInfo.cs
+++ Gu.Wpf.UiAutomation/Properties/AssemblyInfo.cs
@@ -1,3 +1,21 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+[assembly: AssemblyTitle("Gu.Wpf.UiAutomation")]
+[assembly: AssemblyDescription("Library for writing UI-tests for WPF.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johan Larsson")]
+[assembly: AssemblyProduct("Gu.Wpf.UiAutomation")]
+[assembly: AssemblyCopyright("Copyright © Johan Larsson 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1.0-dev")]
 [assembly: InternalsVisibleTo("Gu.Wpf.UiAutomation.UnitTests")]
