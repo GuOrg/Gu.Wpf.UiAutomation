54fc942c9070b827b7f2eab488346471eca90358
JohanLarsson <johan_gavle1@hotmail.com>
2017-08-29 15:30:25 +0200

    Amend.

--- TestApplications/WpfApplication/Windows/Dialog.xaml.cs
+++ TestApplications/WpfApplication/Windows/Dialog.xaml.cs
@@ -1,27 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace WpfApplication.Windows
+﻿namespace WpfApplication.Windows
 {
-    /// <summary>
-    /// Interaction logic for Dialog.xaml
-    /// </summary>
+    using System.Windows;
+
     public partial class Dialog : Window
     {
         public Dialog()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
