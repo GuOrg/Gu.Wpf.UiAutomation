4396ac3ee254b7d96f05663a2ff689591caf225b
JohanLarsson <johan_gavle1@hotmail.com>
2017-08-31 17:29:38 +0200

    Tweak NotepadTests

--- Gu.Wpf.UiAutomation.UITests/NotepadTests.cs
+++ Gu.Wpf.UiAutomation.UITests/NotepadTests.cs
@@ -7,69 +7,69 @@
     public class NotepadTests
     {
         [Test]
-        public void NotepadLaunchTest()
+        public void Launch()
         {
             using (var app = Application.Launch("notepad.exe"))
             {
                 var window = app.MainWindow();
-                Assert.That(window, Is.Not.Null);
-                Assert.That(window.Title, Is.Not.Null);
+                Assert.NotNull(window);
+                Assert.NotNull(window.Title);
             }
         }
 
         [Test]
-        public void NotepadAttachByNameTest()
+        public void Attach()
         {
             using (Application.Launch("notepad.exe"))
             {
                 using (var app = Application.Attach("notepad.exe"))
                 {
                     var window = app.MainWindow();
-                    Assert.That(window, Is.Not.Null);
-                    Assert.That(window.Title, Is.Not.Null);
+                    Assert.NotNull(window);
+                    Assert.NotNull(window.Title);
                 }
             }
         }
 
         [Test]
-        public void NotepadAttachByProcessIdTest()
+        public void AttachProcessId()
         {
             using (var launchedApp = Application.Launch("notepad.exe"))
             {
                 using (var app = Application.Attach(launchedApp.ProcessId))
                 {
                     var window = app.MainWindow();
-                    Assert.That(window, Is.Not.Null);
-                    Assert.That(window.Title, Is.Not.Null);
-                }
-            }
-        }
-
-        [TestCase(@"C:\WINDOWS\system32\notepad.exe")]
-        [TestCase("notepad.exe")]
-        public void NotepadAttachOrLauchIdTest(string name)
-        {
-            using (Application.Launch("notepad.exe"))
-            {
-                using (var app = Application.AttachOrLaunch(new ProcessStartInfo(name)))
-                {
-                    var window = app.MainWindow();
-                    Assert.That(window, Is.Not.Null);
-                    Assert.That(window.Title, Is.Not.Null);
+                    Assert.NotNull(window);
+                    Assert.NotNull(window.Title);
                 }
             }
         }
 
         [Test]
-        public void NotepadAttachWithAbsoluteExePath()
+        public void AttachWithAbsoluteExePath()
         {
             using (Application.Launch("notepad.exe"))
             {
                 using (var app = Application.Attach(@"C:\WINDOWS\system32\notepad.exe"))
                 {
                     var window = app.MainWindow();
-                    Assert.That(window, Is.Not.Null);
-                    Assert.That(window.Title, Is.Not.Null);
+                    Assert.NotNull(window);
+                    Assert.NotNull(window.Title);
+                }
+            }
+        }
+
+        [TestCase(@"C:\WINDOWS\system32\notepad.exe")]
+        [TestCase("notepad.exe")]
+        public void AttachOrLaunch(string name)
+        {
+            using (Application.Launch("notepad.exe"))
+            {
+                using (var app = Application.AttachOrLaunch(new ProcessStartInfo(name)))
+                {
+                    var window = app.MainWindow();
+                    Assert.NotNull(window);
+                    Assert.NotNull(window.Title);
                 }
             }
         }
