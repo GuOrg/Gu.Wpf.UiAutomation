bdb74aa76ca215cb6352400a0b4756b850734903
Johan Larsson <johan_gavle1@hotmail.com>
2017-08-31 23:05:12 +0200

    Refactor tests.

--- Gu.Wpf.UiAutomation.UnitTests/Extensions/EnumeranbleExtTests.cs
+++ Gu.Wpf.UiAutomation.UnitTests/Extensions/EnumeranbleExtTests.cs
@@ -1,28 +1,36 @@
 ﻿namespace Gu.Wpf.UiAutomation.UnitTests.Extensions
 {
+    using System;
+    using System.Linq;
     using NUnit.Framework;
 
     [TestFixture]
     public class EnumeranbleExtTests
     {
-        [TestCase(new int[0], false, 0)]
-        [TestCase(new[] { 1, 2 }, false, 0)]
-        [TestCase(new[] { 1 }, true, 1)]
-        public void TryGetSingle(int[] ints, bool expected, int match)
+        [TestCase("", false, 0)]
+        [TestCase("1, 2", false, 0)]
+        [TestCase("1", true, 1)]
+        public void TryGetSingle(string text, bool expected, int match)
         {
+            var ints = text.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                           .Select(int.Parse)
+                           .ToArray();
             Assert.AreEqual(expected, ints.TryGetSingle(out var result));
             Assert.AreEqual(match, result);
         }
 
-        [TestCase(new int[0], false, 0)]
-        [TestCase(new[] { 2, 2, 3 }, false, 0)]
-        [TestCase(new[] { 1, 1 }, false, 0)]
-        [TestCase(new[] { 1, 1, 3 }, false, 0)]
-        [TestCase(new[] { 1, 2, 2 }, true, 1)]
-        [TestCase(new[] { 1, 2 }, true, 1)]
-        [TestCase(new[] { 1 }, true, 1)]
-        public void TryGetSingleWithSelector(int[] ints, bool expected, int match)
+        [TestCase("", false, 0)]
+        [TestCase("2, 2, 3", false, 0)]
+        [TestCase("1, 1", false, 0)]
+        [TestCase("1, 1, 3", false, 0)]
+        [TestCase("1, 2, 2", true, 1)]
+        [TestCase("1, 2", true, 1)]
+        [TestCase("1", true, 1)]
+        public void TryGetSingleWithSelector(string text, bool expected, int match)
         {
+            var ints = text.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
             Assert.AreEqual(expected, ints.TryGetSingle(x => x == 1, out var result));
             Assert.AreEqual(match, result);
         }
