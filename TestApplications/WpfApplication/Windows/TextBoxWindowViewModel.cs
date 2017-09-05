731a5d69535c3a8dad238aaa010e392af1e480ff
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-05 16:12:48 +0200

    Tweak TextBoxWindow

---
+++ TestApplications/WpfApplication/Windows/TextBoxWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace WpfApplication.Windows
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class TextBoxWindowViewModel : INotifyPropertyChanged
+    {
+        private string text = "abc";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get => this.text;
+
+            set
+            {
+                if (value == this.text)
+                {
+                    return;
+                }
+
+                this.text = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
