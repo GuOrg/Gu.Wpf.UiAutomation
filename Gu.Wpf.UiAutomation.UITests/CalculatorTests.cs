2ee42a0177d878301da9bd20f8746e2a0eefbb11
Johan Larsson <johan_gavle1@hotmail.com>
2017-09-10 17:49:19 +0200

    Refactor CalculatorTest

--- Gu.Wpf.UiAutomation.UITests/CalculatorTests.cs
+++ Gu.Wpf.UiAutomation.UITests/CalculatorTests.cs
@@ -1,12 +1,38 @@
 ﻿namespace Gu.Wpf.UiAutomation.UITests
 {
     using System;
+    using System.Text.RegularExpressions;
     using NUnit.Framework;
     using OperatingSystem = Gu.Wpf.UiAutomation.OperatingSystem;
 
     [TestFixture]
     public class CalculatorTests
     {
+        private interface ICalculator
+        {
+            Button Button1 { get; }
+
+            Button Button2 { get; }
+
+            Button Button3 { get; }
+
+            Button Button4 { get; }
+
+            Button Button5 { get; }
+
+            Button Button6 { get; }
+
+            Button Button7 { get; }
+
+            Button Button8 { get; }
+
+            Button ButtonAdd { get; }
+
+            Button ButtonEquals { get; }
+
+            string Result { get; }
+        }
+
         [Test]
         public void CalculatorTest()
         {
@@ -63,6 +89,86 @@
             }
 
             return Application.Launch("calc.exe");
+        }
+
+        public class Win10Calc : ICalculator
+        {
+            private readonly AutomationElement mainWindow;
+
+            public Win10Calc(AutomationElement mainWindow)
+            {
+                this.mainWindow = mainWindow;
+            }
+
+            public Button Button1 => this.mainWindow.FindButton("num1Button");
+
+            public Button Button2 => this.mainWindow.FindButton("num2Button");
+
+            public Button Button3 => this.mainWindow.FindButton("num3Button");
+
+            public Button Button4 => this.mainWindow.FindButton("num4Button");
+
+            public Button Button5 => this.mainWindow.FindButton("num5Button");
+
+            public Button Button6 => this.mainWindow.FindButton("num6Button");
+
+            public Button Button7 => this.mainWindow.FindButton("num7Button");
+
+            public Button Button8 => this.mainWindow.FindButton("num8Button");
+
+            public Button ButtonAdd => this.mainWindow.FindButton("plusButton");
+
+            public Button ButtonEquals => this.mainWindow.FindButton("equalButton");
+
+            public string Result
+            {
+                get
+                {
+                    var resultElement = this.mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("CalculatorResults"));
+                    var value = resultElement.Properties.Name;
+                    return Regex.Replace(value.Value, "[^0-9]", string.Empty);
+                }
+            }
+        }
+
+        private class LegacyCalc : ICalculator
+        {
+            private readonly AutomationElement mainWindow;
+
+            public LegacyCalc(AutomationElement mainWindow)
+            {
+                this.mainWindow = mainWindow;
+            }
+
+            public Button Button1 => this.mainWindow.FindButton("1");
+
+            public Button Button2 => this.mainWindow.FindButton("2");
+
+            public Button Button3 => this.mainWindow.FindButton("3");
+
+            public Button Button4 => this.mainWindow.FindButton("4");
+
+            public Button Button5 => this.mainWindow.FindButton("5");
+
+            public Button Button6 => this.mainWindow.FindButton("6");
+
+            public Button Button7 => this.mainWindow.FindButton("7");
+
+            public Button Button8 => this.mainWindow.FindButton("8");
+
+            public Button ButtonAdd => this.mainWindow.FindButton("Add");
+
+            public Button ButtonEquals => this.mainWindow.FindButton("Equals");
+
+            public string Result
+            {
+                get
+                {
+                    var resultElement = this.mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("158"));
+                    var value = resultElement.Properties.Name;
+                    return Regex.Replace(value.Value, "[^0-9]", string.Empty);
+                }
+            }
         }
     }
 }
