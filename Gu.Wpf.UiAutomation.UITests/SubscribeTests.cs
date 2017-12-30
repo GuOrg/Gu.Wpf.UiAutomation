abb78252231f1fe5090d299c4352fc6c0f5e5869
Johan Larsson <johan_gavle1@hotmail.com>
2017-12-30 21:27:11 +0100

    SubscribeTests

--- Gu.Wpf.UiAutomation.UITests/SubscribeTests.cs
+++ Gu.Wpf.UiAutomation.UITests/SubscribeTests.cs
@@ -3,19 +3,63 @@
     using System;
     using System.Collections.Generic;
     using System.Globalization;
+    using System.Windows.Automation;
     using NUnit.Framework;
 
     [TestFixture]
     public class SubscribeTests
     {
+        private const string ExeFileName = "WpfApplication.exe";
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Application.KillLaunched(ExeFileName);
+        }
+
+        [Test]
+        public void FocusWindowFocusChanges()
+        {
+            using (var app = Application.AttachOrLaunch(ExeFileName, "FocusWindow"))
+            {
+                var changes = new List<string>();
+                var window = app.MainWindow;
+                using (Subscribe.ToFocusChangedEvent(element => changes.Add(element.Name)))
+                {
+                    window.FindTextBox("TextBox2").Focus();
+                    CollectionAssert.AreEqual(new[] { "TextBox2" }, changes);
+
+                    window.FindTextBox("Button1").Focus();
+                    CollectionAssert.AreEqual(new[] { "Button1" }, changes);
+                }
+            }
+        }
+
+        [Test]
+        public void FocusWindowPropertyChanges()
+        {
+            using (var app = Application.AttachOrLaunch(ExeFileName, "FocusWindow"))
+            {
+                var changes = new List<string>();
+                var window = app.MainWindow;
+                var textBox = window.FindTextBox("TextBox1");
+
+                using (Subscribe.ToPropertyChangedEvent(textBox.AutomationElement, TreeScope.Element, ValuePattern.ValueProperty, (e, p, o) => changes.Add($"{e.AutomationId}.{p.ProgrammaticName.Split('.')[1]} = {o}")))
+                {
+                    textBox.Text = "abc";
+                    CollectionAssert.AreEqual(new[] { "TextBox1.ValueProperty = abc" }, changes);
+                }
+            }
+        }
+
         [Test]
         public void FocusChangedWithPaintTest()
         {
             using (var app = Application.Launch("mspaint"))
             {
-                var focusChangedElements = new List<string>();
+                var changes = new List<string>();
                 var mainWindow = app.MainWindow;
-                using (Subscribe.ToFocusChangedEvent(element => { focusChangedElements.Add(element.ToString()); }))
+                using (Subscribe.ToFocusChangedEvent(element => changes.Add(element.ToString())))
                 {
                     Wait.For(TimeSpan.FromMilliseconds(100));
                     var button1 = mainWindow.FindButton(this.GetResizeText());
@@ -28,7 +72,7 @@
                     {
                         Wait.For(TimeSpan.FromMilliseconds(100));
                         mainWindow.Close();
-                        Assert.That(focusChangedElements.Count, Is.GreaterThan(0));
+                        Assert.That(changes.Count, Is.GreaterThan(0));
                     }
                 }
             }
