e88da5eac7888ea50cac947e0330c3ed7597fac3
JohanLarsson <johan_gavle1@hotmail.com>
2017-12-31 13:28:04 +0100

    Tests for ToolBar & StatusBar

---
+++ Gu.Wpf.UiAutomation.UiTests/Elements/StatusBarTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Gu.Wpf.UiAutomation.UiTests.Elements
+{
+    using NUnit.Framework;
+
+    public class StatusBarTests
+    {
+        private const string ExeFileName = "WpfApplication.exe";
+
+        [Test]
+        public void Find()
+        {
+            using (var app = Application.Launch(ExeFileName, "StatusBarWindow"))
+            {
+                var window = app.MainWindow;
+                var statusBar = window.FindStatusBar();
+                Assert.NotNull(statusBar);
+            }
+        }
+    }
+}
