8e432fc4b6bfc9b0c7f4c9c8b9c64e5043af77f9
Johan Larsson <johan_gavle1@hotmail.com>
2017-12-31 17:32:02 +0100

    SandboxTests

--- Gu.Wpf.UiAutomation.UITests/SandboxTests.cs
+++ Gu.Wpf.UiAutomation.UITests/SandboxTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Gu.Wpf.UiAutomation.UiTests
 {
     using System;
+    using System.Linq;
     using System.Windows.Automation;
     using NUnit.Framework;
 
@@ -8,26 +9,91 @@
     public class SandboxTests
     {
         [Test]
-        public void Dump()
+        public void DumpTabControl()
         {
-            using (var app = Application.Launch("WpfApplication.exe", "DatePickerWindow"))
+            using (var app = Application.Launch("WpfApplication.exe", "TabControlWindow"))
             {
                 var window = app.MainWindow;
-                var element = window.AutomationElement.FindIndexed(TreeScope.Children, Condition.TrueCondition, 1);
-                Console.WriteLine($"ControlType: {element.Current.ControlType.ProgrammaticName}");
-                Console.WriteLine($"LocalizedControlType: {element.Current.LocalizedControlType}");
-                Console.WriteLine($"ClassName: {element.Current.ClassName}");
-                Console.WriteLine($"IsContentElement: {element.Current.IsContentElement}");
-                Console.WriteLine($"IsControlElement: {element.Current.IsControlElement}");
+                var element = window.FindTabControl().AutomationElement;
+                DumpRecursive(element);
+            }
+        }
 
+        [Test]
+        public void DumpTabItem()
+        {
+            using (var app = Application.Launch("WpfApplication.exe", "TabControlWindow"))
+            {
+                var window = app.MainWindow;
+                var element = window.FindTabControl().Items[0].AutomationElement;
+                DumpRecursive(element);
+            }
+        }
+
+        [Test]
+        public void DumpGroupBox()
+        {
+            using (var app = Application.Launch("WpfApplication.exe", "GroupBoxWindow"))
+            {
+                var window = app.MainWindow;
+                var element = window.FindGroupBox().AutomationElement;
+                DumpRecursive(element);
+            }
+        }
+
+        [Test]
+        public void DumpExpander()
+        {
+            using (var app = Application.Launch("WpfApplication.exe", "ExpanderWindow"))
+            {
+                var window = app.MainWindow;
+                var element = window.FindExpander().AutomationElement;
+                DumpRecursive(element);
+            }
+        }
+
+        private static void DumpRecursive(AutomationElement element, bool all = true, string padding = "")
+        {
+            DumpPropertiesAndPatterns(element, all, padding);
+            foreach (var child in element.Children())
+            {
+                DumpRecursive(child, all, padding + "  ");
+                Console.WriteLine();
+            }
+        }
+
+        private static void DumpPropertiesAndPatterns(AutomationElement element, bool all = true, string padding = "")
+        {
+            Console.WriteLine($"{padding}ControlType: {element.Current.ControlType.ProgrammaticName}");
+            Console.WriteLine($"{padding}LocalizedControlType: {element.Current.LocalizedControlType}");
+            Console.WriteLine($"{padding}ClassName: {element.Current.ClassName}");
+            Console.WriteLine($"{padding}AutomationId: {element.Current.AutomationId}");
+            Console.WriteLine($"{padding}IsContentElement: {element.Current.IsContentElement}");
+            Console.WriteLine($"{padding}IsControlElement: {element.Current.IsControlElement}");
+
+            if (all)
+            {
                 foreach (var pattern in element.GetSupportedPatterns())
                 {
-                    Console.WriteLine(pattern.ProgrammaticName);
+                    Console.WriteLine($"{padding}{pattern.ProgrammaticName}");
+                    var currentPattern = element.GetCurrentPattern(pattern);
+                    Console.WriteLine($"{padding}{currentPattern}");
+                    var currentProperty = currentPattern.GetType().GetProperty("Current");
+                    if (currentProperty != null)
+                    {
+                        var value = currentProperty.GetValue(currentPattern);
+                        foreach (var property in value.GetType().GetProperties())
+                        {
+                            Console.WriteLine($"{padding}{property.Name} {property.GetValue(value)}");
+                        }
+                    }
+
+                    Console.WriteLine();
                 }
 
-                foreach (var property in element.GetSupportedProperties())
+                foreach (var property in element.GetSupportedProperties().OrderBy(x => x.ProgrammaticName))
                 {
-                    Console.WriteLine(property.ProgrammaticName);
+                    Console.WriteLine($"{padding}{property.ProgrammaticName.TrimStart("AutomationElementIdentifiers.").TrimEnd("Property")} {element.GetCurrentPropertyValue(property)}");
                 }
             }
         }
