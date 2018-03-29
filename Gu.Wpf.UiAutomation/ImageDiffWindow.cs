ca90fb3ae96b908d9ef17d2c2b26570da084b053
JohanLarsson <johan_gavle1@hotmail.com>
2018-03-29 14:39:24 +0200

    Tweak image diff window, fix #27.

--- Gu.Wpf.UiAutomation/ImageDiffWindow.cs
+++ Gu.Wpf.UiAutomation/ImageDiffWindow.cs
@@ -1,12 +1,16 @@
 // ReSharper disable RedundantNameQualifier
 namespace Gu.Wpf.UiAutomation
 {
+    using System;
     using System.Drawing;
+    using System.Globalization;
     using System.IO;
     using System.Threading;
     using System.Windows;
     using System.Windows.Automation;
     using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Data;
     using System.Windows.Media.Imaging;
     using System.Windows.Threading;
 
@@ -14,6 +18,8 @@ namespace Gu.Wpf.UiAutomation
     {
         private readonly System.Windows.Controls.Image expectedImage;
         private readonly System.Windows.Controls.Image actualImage;
+        private readonly System.Windows.Controls.Primitives.ToggleButton expectedButton;
+        private readonly System.Windows.Controls.Primitives.ToggleButton actualButton;
 
         private ImageDiffWindow(Bitmap expected, Bitmap actual)
         {
@@ -25,6 +31,7 @@ namespace Gu.Wpf.UiAutomation
             this.ShowInTaskbar = false;
             this.Height = 400;
             this.Width = 400;
+            this.Title = "Image diff";
             var root = new Grid();
             root.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
             root.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
@@ -34,11 +41,75 @@ namespace Gu.Wpf.UiAutomation
             this.actualImage = this.CreateImage(actual);
             root.Children.Add(this.actualImage);
 
-            var visibilityButton = new System.Windows.Controls.Primitives.RepeatButton { Content = "Toggle visibility" };
-            visibilityButton.Click += this.OnVisibilityButtonClick;
-            Grid.SetRow(visibilityButton, 1);
-            root.Children.Add(visibilityButton);
+            var buttonGrid = new UniformGrid
+            {
+                Rows = 1,
+            };
+            Grid.SetRow(buttonGrid, 1);
+            this.expectedButton = new System.Windows.Controls.Primitives.ToggleButton
+            {
+                Content = "Expected",
+                IsChecked = true,
+            };
+            BindingOperations.SetBinding(
+                this.expectedImage,
+                VisibilityProperty,
+                new Binding
+                {
+                    Path = new PropertyPath("IsChecked"),
+                    Mode = BindingMode.TwoWay,
+                    Converter = new BooleanToVisibilityConverter(),
+                    Source = this.expectedButton,
+                });
+            buttonGrid.Children.Add(this.expectedButton);
+
+            this.actualButton = new System.Windows.Controls.Primitives.ToggleButton
+            {
+                Content = "actual",
+                IsChecked = true,
+            };
+            BindingOperations.SetBinding(
+                this.actualImage,
+                VisibilityProperty,
+                new Binding
+                {
+                    Path = new PropertyPath("IsChecked"),
+                    Mode = BindingMode.TwoWay,
+                    Converter = new BooleanToVisibilityConverter(),
+                    Source = this.actualButton,
+                });
+            buttonGrid.Children.Add(this.actualButton);
+            root.Children.Add(buttonGrid);
+
+            this.BindOpacity(this.expectedImage);
+            this.BindOpacity(this.actualImage);
             this.Content = root;
+        }
+
+        private void BindOpacity(System.Windows.Controls.Image image)
+        {
+            BindingOperations.SetBinding(
+                image,
+                OpacityProperty,
+                new MultiBinding
+                {
+                    Converter = new AndToOpacityConverter(),
+                    Bindings =
+                    {
+                        new Binding
+                        {
+                            Path = new PropertyPath("IsChecked"),
+                            Mode = BindingMode.OneWay,
+                            Source = this.expectedButton,
+                        },
+                        new Binding
+                        {
+                            Path = new PropertyPath("IsChecked"),
+                            Mode = BindingMode.OneWay,
+                            Source = this.actualButton,
+                        },
+                    }
+                });
         }
 
         public static void Show(Bitmap expected, Bitmap actual)
@@ -106,26 +177,23 @@ namespace Gu.Wpf.UiAutomation
             return image;
         }
 
-        private void OnVisibilityButtonClick(object sender, RoutedEventArgs routedEventArgs)
+        private class AndToOpacityConverter : IMultiValueConverter
         {
-            if (this.expectedImage.Visibility == Visibility.Visible &&
-                this.actualImage.Visibility == Visibility.Visible)
+            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
             {
-                this.actualImage.Visibility = Visibility.Hidden;
-                this.expectedImage.Opacity = 1;
+                if (values.Length == 2 &&
+                    values[0] is bool b0 &&
+                    values[1] is bool b1)
+                {
+                    return b0 && b1 ? 0.5 : 1;
+                }
+
+                return 1;
             }
-            else if (this.actualImage.Visibility == Visibility.Hidden)
+
+            object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
             {
-                this.actualImage.Visibility = Visibility.Visible;
-                this.actualImage.Opacity = 1;
-                this.expectedImage.Visibility = Visibility.Hidden;
-            }
-            else if (this.expectedImage.Visibility == Visibility.Hidden)
-            {
-                this.actualImage.Visibility = Visibility.Visible;
-                this.actualImage.Opacity = 0.5;
-                this.expectedImage.Visibility = Visibility.Visible;
-                this.expectedImage.Opacity = 0.5;
+                throw new NotImplementedException();
             }
         }
     }
