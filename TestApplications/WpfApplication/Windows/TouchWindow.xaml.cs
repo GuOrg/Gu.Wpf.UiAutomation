f4cdba3ba3d1164e758782bd3c8a66b0c6eecbbb
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-24 12:01:14 +0200

    TouchWindow

---
+++ TestApplications/WpfApplication/Windows/TouchWindow.xaml.cs
@@ -0,0 +1,19 @@
+namespace WpfApplication.Windows
+{
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public partial class TouchWindow : Window
+    {
+        public TouchWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnTouch(object sender, TouchEventArgs e)
+        {
+            this.Events.Items.Add($"{e.RoutedEvent.Name} Position: {e.GetTouchPoint((IInputElement)e.Source).Position.ToString(CultureInfo.InvariantCulture)}");
+        }
+    }
+}
