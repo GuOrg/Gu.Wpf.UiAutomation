7ce64e5ccdf022ea686fad2b3871398189730f63
JohanLarsson <johan_gavle1@hotmail.com>
2019-01-06 20:34:18 +0100

    Formatting.

--- Gu.Wpf.UiAutomation/WindowsAPI/INPUT.cs
+++ Gu.Wpf.UiAutomation/WindowsAPI/INPUT.cs
@@ -1,5 +1,7 @@
 // ReSharper disable InconsistentNaming
 #pragma warning disable CA1815 // Override equals and operator equals on value types
+#pragma warning disable SA1307 // Accessible fields must begin with upper-case letter
+#pragma warning disable CA1051 // Do not declare visible instance fields
 namespace Gu.Wpf.UiAutomation.WindowsAPI
 {
     using System.Runtime.InteropServices;
@@ -7,28 +9,36 @@ namespace Gu.Wpf.UiAutomation.WindowsAPI
     [StructLayout(LayoutKind.Sequential)]
     public struct INPUT
     {
-#pragma warning disable SA1307 // Accessible fields must begin with upper-case letter
-#pragma warning disable CA1051 // Do not declare visible instance fields
         public InputType type;
         public INPUTUNION u;
-#pragma warning restore CA1051 // Do not declare visible instance fields
-#pragma warning restore SA1307 // Accessible fields must begin with upper-case letter
 
         public static int Size => Marshal.SizeOf(typeof(INPUT));
 
         public static INPUT MouseInput(MOUSEINPUT mouseInput)
         {
-            return new INPUT { type = InputType.INPUT_MOUSE, u = new INPUTUNION { mi = mouseInput } };
+            return new INPUT
+            {
+                type = InputType.INPUT_MOUSE,
+                u = new INPUTUNION { mi = mouseInput },
+            };
         }
 
         public static INPUT KeyboardInput(KEYBDINPUT keyboardInput)
         {
-            return new INPUT { type = InputType.INPUT_KEYBOARD, u = new INPUTUNION { ki = keyboardInput } };
+            return new INPUT
+            {
+                type = InputType.INPUT_KEYBOARD,
+                u = new INPUTUNION { ki = keyboardInput },
+            };
         }
 
         public static INPUT HardwareInput(HARDWAREINPUT hardwareInput)
         {
-            return new INPUT { type = InputType.INPUT_HARDWARE, u = new INPUTUNION { hi = hardwareInput } };
+            return new INPUT
+            {
+                type = InputType.INPUT_HARDWARE,
+                u = new INPUTUNION { hi = hardwareInput },
+            };
         }
     }
 }
