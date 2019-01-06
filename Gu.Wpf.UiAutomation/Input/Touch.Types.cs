4366ef4cc54926d6946e53e5f20f02cebaf39345
JohanLarsson <johan_gavle1@hotmail.com>
2019-01-06 11:15:41 +0100

    More docs.

--- Gu.Wpf.UiAutomation/Input/Touch.Types.cs
+++ Gu.Wpf.UiAutomation/Input/Touch.Types.cs
@@ -188,18 +188,65 @@ namespace Gu.Wpf.UiAutomation
             PT_TOUCHPAD = 0x00000005,
         }
 
+        /// <summary>
+        /// Identifies a change in the state of a button associated with a pointer.
+        /// https://docs.microsoft.com/en-us/windows/desktop/api/winuser/ne-winuser-tagpointer_button_change_type.
+        /// </summary>
         private enum PointerButtonChangeType
         {
+            /// <summary>
+            /// No change in button state.
+            /// </summary>
             NONE,
+
+            /// <summary>
+            /// The first button (see POINTER_FLAG_FIRSTBUTTON) transitioned to a pressed state.
+            /// </summary>
             FIRSTBUTTON_DOWN,
+
+            /// <summary>
+            /// The first button (see POINTER_FLAG_FIRSTBUTTON) transitioned to a released state.
+            /// </summary>
             FIRSTBUTTON_UP,
+
+            /// <summary>
+            /// The second button (see POINTER_FLAG_SECONDBUTTON) transitioned to a pressed state.
+            /// </summary>
             SECONDBUTTON_DOWN,
+
+            /// <summary>
+            /// The second button (see POINTER_FLAG_SECONDBUTTON) transitioned to a released state.
+            /// </summary>
             SECONDBUTTON_UP,
+
+            /// <summary>
+            /// The third button (see POINTER_FLAG_THIRDBUTTON) transitioned to a pressed state.
+            /// </summary>
             THIRDBUTTON_DOWN,
+
+            /// <summary>
+            /// The third button (see POINTER_FLAG_THIRDBUTTON) transitioned to a released state.
+            /// </summary>
             THIRDBUTTON_UP,
+
+            /// <summary>
+            /// The fourth button (see POINTER_FLAG_FOURTHBUTTON) transitioned to a pressed state.
+            /// </summary>
             FOURTHBUTTON_DOWN,
+
+            /// <summary>
+            /// The fourth button (see POINTER_FLAG_FOURTHBUTTON) transitioned to a released state.
+            /// </summary>
             FOURTHBUTTON_UP,
+
+            /// <summary>
+            /// The fifth button (see POINTER_FLAG_FIFTHBUTTON) transitioned to a pressed state.
+            /// </summary>
             FIFTHBUTTON_DOWN,
+
+            /// <summary>
+            /// The fifth button (see POINTER_FLAG_FIFTHBUTTON) transitioned to a released state.
+            /// </summary>
             FIFTHBUTTON_UP,
         }
 
@@ -266,17 +313,83 @@ namespace Gu.Wpf.UiAutomation
             /// May be any reasonable combination of flags from the <see cref="PointerFlag"/> constants.
             /// </summary>
             public PointerFlag PointerFlags;
+
+            /// <summary>
+            /// Handle to the source device that can be used in calls to the raw input device API and the digitizer device API.
+            /// </summary>
             public IntPtr SourceDevice;
+
+            /// <summary>
+            /// Window to which this message was targeted.
+            /// If the pointer is captured, either implicitly by virtue of having made contact over this window or explicitly using the pointer capture API, this is the capture window.
+            /// If the pointer is uncaptured, this is the window over which the pointer was when this message was generated.
+            /// </summary>
             public IntPtr HwndTarget;
+
+            /// <summary>
+            /// The predicted screen coordinates of the pointer, in pixels.
+            /// The predicted value is based on the pointer position reported by the digitizer and the motion of the pointer.
+            /// This correction can compensate for visual lag due to inherent delays in sensing and processing the pointer location on the digitizer.This is applicable to pointers of type PT_TOUCH.
+            /// For other pointer types, the predicted value will be the same as the non-predicted value (see ptPixelLocationRaw).
+            /// </summary>
             public TouchPoint PtPixelLocation;
+
+            /// <summary>
+            /// The predicted screen coordinates of the pointer, in HIMETRIC units.
+            /// The predicted value is based on the pointer position reported by the digitizer and the motion of the pointer.
+            /// This correction can compensate for visual lag due to inherent delays in sensing and processing the pointer location on the digitizer.
+            /// This is applicable to pointers of type PT_TOUCH.For other pointer types, the predicted value will be the same as the non-predicted value (see ptHimetricLocationRaw).
+            /// </summary>
             public TouchPoint PtPixelLocationRaw;
+
+            /// <summary>
+            /// The screen coordinates of the pointer, in pixels. For adjusted screen coordinates, see ptPixelLocation.
+            /// </summary>
             public TouchPoint PtHimetricLocation;
+
+            /// <summary>
+            /// The screen coordinates of the pointer, in HIMETRIC units. For adjusted screen coordinates, see ptHimetricLocation.
+            /// </summary>
             public TouchPoint PtHimetricLocationRaw;
+
+            /// <summary>
+            /// 0 or the time stamp of the message, based on the system tick count when the message was received.
+            /// The application can specify the input time stamp in either dwTime or PerformanceCount.
+            /// The value cannot be more recent than the current tick count or QueryPerformanceCount (QPC) value of the injection thread. Once a frame is injected with a time stamp, all subsequent frames must include a timestamp until all contacts in the frame go to an UP state.The custom timestamp value must also be provided for the first element in the contacts array.The time stamp values after the first element are ignored. The custom timestamp value must increment in every injection frame.
+            /// When PerformanceCount is specified, the time stamp will be converted to the current time in .1 millisecond resolution upon actual injection.If a custom PerformanceCount resulted in the same .1 millisecond window from the previous injection, ERROR_NOT_READY is returned and injection will not occur. While injection will not be invalidated immediately by the error, the next successful injection must have a PerformanceCount value that is at least 0.1 millisecond from the previously successful injection. This is also true if dwTime is used.
+            /// If both dwTime and PerformanceCount are specified in InjectTouchInput, ERROR_INVALID_PARAMETER is returned.
+            /// InjectTouchInput cannot switch between dwTime and PerformanceCount once injection has started.
+            /// If neither dwTime and PerformanceCount are specified, InjectTouchInput allocates the timestamp based on the timing of the call.
+            /// If InjectTouchInput calls are repeatedly less than 0.1 millisecond apart, ERROR_NOT_READY might be returned.
+            /// The error will not invalidate the input immediately, but the injection application needs to retry the same frame again for injection to succeed.
+            /// </summary>
             public uint DwTime;
+
+            /// <summary>
+            /// Count of inputs that were coalesced into this message.
+            /// This count matches the total count of entries that can be returned by a call to GetPointerInfoHistory.
+            /// If no coalescing occurred, this count is 1 for the single input represented by the message.
+            /// </summary>
             public uint HistoryCount;
+
             public uint InputData;
+
+            /// <summary>
+            /// Indicates which keyboard modifier keys were pressed at the time the input was generated. May be zero or a combination of the following values.
+            /// POINTER_MOD_SHIFT – A SHIFT key was pressed.
+            /// POINTER_MOD_CTRL – A CTRL key was pressed.
+            /// </summary>
             public uint DwKeyStates;
+
+            /// <summary>
+            /// The value of the high-resolution performance counter when the pointer message was received (high-precision, 64 bit alternative to dwTime).
+            /// The value can be calibrated when the touch digitizer hardware supports the scan timestamp information in its input report.
+            /// </summary>
             public ulong PerformanceCount;
+
+            /// <summary>
+            /// A value from the POINTER_BUTTON_CHANGE_TYPE enumeration that specifies the change in button state between this input and the previous input.
+            /// </summary>
             public PointerButtonChangeType ButtonChangeType;
         }
 
